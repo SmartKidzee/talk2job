cded87445bd066705e8c7e95f8dac0ee7a45ca61
SmartKidzee <shreyas12.kidzee@gmail.com>
2025-04-20 16:08:32 +0530

    first commit

---
+++ firebase/client.ts
@@ -0,0 +1,20 @@
+
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_98rS7vqkfip3llk5T6Qpo3IAtU-Bb98",
+    authDomain: "talk2jobshrey.firebaseapp.com",
+    projectId: "talk2jobshrey",
+    storageBucket: "talk2jobshrey.firebasestorage.app",
+    messagingSenderId: "185401160189",
+    appId: "1:185401160189:web:ac26fdde4d2cbed677a994",
+    measurementId: "G-LQWGFZENBN"
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app)
